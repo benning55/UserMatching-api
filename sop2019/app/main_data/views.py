73054c5215cce7eaceeeabfc8e0296c285ba4008
benning55 <60070109@kmitl.ac.th>
2019-11-07 10:52:15 +0700

    fetch data from service

--- sop2019/app/main_data/views.py
+++ sop2019/app/main_data/views.py
@@ -1,6 +1,7 @@
 import urllib
 import urllib.request
 import json
+import requests
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
@@ -26,8 +27,10 @@ def person_view(request):
             print(f'{result.first_name} {result.last_name}')
             final_data = json.dumps(cls=MyEncoder, obj=result)
             data_a = get_company_a_data()
+            print(data_a)
             # print(data_a)
             data_b = get_company_b_data()
+            print(data_b)
             payload = {
                 'companyA': data_a,
                 'companyB': data_b
@@ -40,32 +43,35 @@ def person_view(request):
 @permission_classes([AllowAny, ])
 def companya_data(request):
     """Look at company a data"""
-    url = "http://192.168.1.37:8200/companya/"
+    url = "http://10.110.198.242:8200/companya/search-person/"
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
     print("Test what is happen")
-    context = {
-        "Benning": "MUSE"
+    payload = {
+        "first_name": "ben",
+        "last_name": "maisonti"
     }
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
-    result = data[0]
-    print(data)
-    print(data[0])
-    return Response(result, status=status.HTTP_200_OK)
+    res = requests.post(url, json=payload, headers=headers)
+    print(res.json())
+    return Response(res.json(), status=status.HTTP_200_OK)
 
 
 def get_company_a_data():
     """Get the data from company A"""
-    url = "http://192.168.1.37:8200/companya/"
+    url = "http://10.110.198.242:8200/companya/"
     response = urllib.request.urlopen(url)
     data = json.loads(response.read())
-    return data[0]
+    return data
 
 
 def get_company_b_data():
     """Get the data from company B"""
-    url = "http://192.168.1.37:8200/companya/"
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
-    return data[0]
-
+    url = "http://10.110.198.242:8200/companya/search-person/"
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+    print("Test what is happen")
+    payload = {
+        "first_name": "muse",
+        "last_name": "muse"
+    }
+    res = requests.post(url, json=payload, headers=headers)
+    return res.json()
 
